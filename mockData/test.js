750f6996f7339f44c347e070c46e4a1467d22a48
orna <or0naa@gmail.com>
2024-05-28 16:46:38 +0300

    save fake data. without logic for now

--- mockData/test.js
+++ mockData/test.js
@@ -19,23 +19,23 @@ async function go() {
         "https://facebook.com/yossi",
         "https://twitter.com/yossi"
       ],
-      "my_challenges": ["challenge1", "challenge2"],
+      "my_challenges": [],
       "coins": 100,
       "notifications": [
-        {
-          "challenge": "challenge1",
-          "type": "join",
-          "title": "Joined Challenge",
-          "content": "Yossi joined the first challenge",
-          "sender": "Yossi Cohen"
-        },
-        {
-          "challenge": "challenge2",
-          "type": "sent support",
-          "title": "Sent Support",
-          "content": "Yossi sent support for the second challenge",
-          "sender": "Yossi Cohen"
-        }
+        // {
+        //   "challenge": "",
+        //   "type": "join",
+        //   "title": "Joined Challenge",
+        //   "content": "Yossi joined the first challenge",
+        //   "sender": "Yossi Cohen"
+        // },
+        // {
+        //   "challenge": "",
+        //   "type": "sent support",
+        //   "title": "Sent Support",
+        //   "content": "Yossi sent support for the second challenge",
+        //   "sender": "Yossi Cohen"
+        // }
       ]
     },
     {
@@ -49,16 +49,16 @@ async function go() {
         "https://linkedin.com/in/ronit",
         "https://instagram.com/ronit"
       ],
-      "my_challenges": ["challenge3"],
+      "my_challenges": [],
       "coins": 150,
       "notifications": [
-        {
-          "challenge": "challenge3",
-          "type": "sent message",
-          "title": "New Message",
-          "content": "Ronit sent a message in the third challenge",
-          "sender": "Ronit Levy"
-        }
+        // {
+        //   "challenge": "challenge3",
+        //   "type": "sent message",
+        //   "title": "New Message",
+        //   "content": "Ronit sent a message in the third challenge",
+        //   "sender": "Ronit Levy"
+        // }
       ]
     },
     {
@@ -72,16 +72,16 @@ async function go() {
         "https://twitter.com/david",
         "https://facebook.com/david"
       ],
-      "my_challenges": ["challenge4", "challenge5"],
+      "my_challenges": [],
       "coins": 200,
       "notifications": [
-        {
-          "challenge": "challenge4",
-          "type": "join",
-          "title": "Joined Challenge",
-          "content": "David joined the fourth challenge",
-          "sender": "David Israeli"
-        }
+        // {
+        //   "challenge": "challenge4",
+        //   "type": "join",
+        //   "title": "Joined Challenge",
+        //   "content": "David joined the fourth challenge",
+        //   "sender": "David Israeli"
+        // }
       ]
     },
     {
@@ -95,17 +95,9 @@ async function go() {
         "https://linkedin.com/in/hana",
         "https://instagram.com/hana"
       ],
-      "my_challenges": ["challenge6"],
+      "my_challenges": [],
       "coins": 250,
-      "notifications": [
-        {
-          "challenge": "challenge6",
-          "type": "sent support",
-          "title": "Sent Support",
-          "content": "Hana sent support for the sixth challenge",
-          "sender": "Hana Shahar"
-        }
-      ]
+      "notifications": []
     }
   ]
 
@@ -115,6 +107,99 @@ async function go() {
   const m4 = await UserModel.create(users[3]);
 
   const members = [m1, m2, m3, m4]
+
+  //await ChallengeModel.create>>
+  let exampleChallenge = {
+    "challenge_name": "Fitness Challenge",
+    "cover_image": "https://www.dietmaster.co.il/wp-content/uploads/2015/02/%D7%A9%D7%99%D7%98%D7%95%D7%AA-%D7%94%D7%A8%D7%96%D7%99%D7%94.jpg",
+    "subtitle": "Get fit in 30 days",
+    "duration_days": 30,
+    "tags": ["fitness", "health", "wellness"],
+    "is_public": true,
+    "is_template": false,
+    "creator": null,  // Relevant if is_template is true
+    "store": [
+      {
+        "name": "Water Bottle",
+        "description": "Stainless steel water bottle",
+        "image": "https://www.ewines.co.il/wp-content/uploads/2022/08/3-%D7%9E%D7%99%D7%9D.jpg",
+        "coins": 50,
+        "days_to_expire": 30,
+        "quantity": 100
+      }
+    ],
+    "cards": [
+      {
+        "day": 1,
+        "card_number": 1,
+        "card_type": "question",
+        "sub_type": "open",
+        "title": "What do you like to eat?",
+        "content": "Long text",
+        "media": {
+          "type": "image",
+          "content": "https://static1.s123-cdn-static-a.com/uploads/1898061/2000_5c88fac711626.jpg"
+        },
+        "coins": 10,
+        "image": "https://www.yo-yoo.co.il/coolpics/images/uploads/sim1.png",
+        "appearance_percentage": 0,  // Only for lottery card
+        "win_percentage": 0  // Only for lottery card
+      }
+    ],
+    "invited": ["example1@example.com", "example2@example.com"]
+  }
+
+  let exampleChallenge2 = {
+    "challenge_name": "Reading Challenge",
+    "cover_image": "https://meyda.education.gov.il/files/pop/10322/kriaa.jpg",
+    "subtitle": "Read 10 books in 60 days",
+    "duration_days": 60,
+    "tags": ["reading", "books", "education"],
+    "is_public": true,
+    "is_template": false,
+    "creator": null,
+    "store": [
+      {
+        "name": "Bookmark",
+        "description": "Elegant bookmark",
+        "image": "https://i.pinimg.com/736x/8c/f7/0b/8cf70b1a4a54c9cda9d40d5db77d75e8.jpg",
+        "coins": 20,
+        "days_to_expire": 60,
+        "quantity": 200
+      }
+    ],
+    "cards": [
+      {
+        "day": 1,
+        "card_number": 1,
+        "card_type": "question",
+        "sub_type": "open",
+        "title": "What is your favorite book?",
+        "content": "Long text",
+        "media": {
+          "type": "text",
+          "content": "The Great Gatsby"
+        },
+        "coins": 5,
+        "image": null,
+        "appearance_percentage": 0,
+        "win_percentage": 0
+      }
+    ],
+    "invited": ["reader1@example.com", "reader2@example.com"]
+  }
+
+
+  members.forEach(async m => {
+    m.notifications.push({
+      "challenge": exampleChallenge._id,
+      "type": "sent support",
+      "title": "Sent Support",
+      "content": `${m.fullName} sent support for the sixth challenge`,
+      "sender": m.fullName
+    })
+    m.save()
+  })
 
   let coachs = [
     {
@@ -126,91 +211,10 @@ async function go() {
 
   ]
 
-  let challenges = [
-    {
-      "challenge_1": {
-        "challenge_name": "Fitness Challenge",
-        "cover_image": "https://www.dietmaster.co.il/wp-content/uploads/2015/02/%D7%A9%D7%99%D7%98%D7%95%D7%AA-%D7%94%D7%A8%D7%96%D7%99%D7%94.jpg",
-        "subtitle": "Get fit in 30 days",
-        "duration_days": 30,
-        "tags": ["fitness", "health", "wellness"],
-        "is_public": true,
-        "is_template": false,
-        "creator": null,  // Relevant if is_template is true
-        "store": [
-          {
-            "name": "Water Bottle",
-            "description": "Stainless steel water bottle",
-            "image": "https://www.ewines.co.il/wp-content/uploads/2022/08/3-%D7%9E%D7%99%D7%9D.jpg",
-            "coins": 50,
-            "days_to_expire": 30,
-            "quantity": 100
-          }
-        ],
-        "cards": [
-          {
-            "day": 1,
-            "card_number": 1,
-            "card_type": "question",
-            "sub_type": "open",
-            "title": "What do you like to eat?",
-            "content": "Long text",
-            "media": {
-              "type": "image",
-              "content": "https://static1.s123-cdn-static-a.com/uploads/1898061/2000_5c88fac711626.jpg"
-            },
-            "coins": 10,
-            "image": "https://www.yo-yoo.co.il/coolpics/images/uploads/sim1.png",
-            "appearance_percentage": 0,  // Only for lottery card
-            "win_percentage": 0  // Only for lottery card
-          }
-        ],
-        "invited": ["example1@example.com", "example2@example.com"]
-      }
-    },
-    {
-      "challenge_2": {
-        "challenge_name": "Reading Challenge",
-        "cover_image": "https://meyda.education.gov.il/files/pop/10322/kriaa.jpg",
-        "subtitle": "Read 10 books in 60 days",
-        "duration_days": 60,
-        "tags": ["reading", "books", "education"],
-        "is_public": true,
-        "is_template": false,
-        "creator": null,
-        "store": [
-          {
-            "name": "Bookmark",
-            "description": "Elegant bookmark",
-            "image": "https://i.pinimg.com/736x/8c/f7/0b/8cf70b1a4a54c9cda9d40d5db77d75e8.jpg",
-            "coins": 20,
-            "days_to_expire": 60,
-            "quantity": 200
-          }
-        ],
-        "cards": [
-          {
-            "day": 1,
-            "card_number": 1,
-            "card_type": "question",
-            "sub_type": "open",
-            "title": "What is your favorite book?",
-            "content": "Long text",
-            "media": {
-              "type": "text",
-              "content": "The Great Gatsby"
-            },
-            "coins": 5,
-            "image": null,
-            "appearance_percentage": 0,
-            "win_percentage": 0
-          }
-        ],
-        "invited": ["reader1@example.com", "reader2@example.com"]
-      }
-    }
 
-  ]
+
+
+
   console.log("###########  END  ##########");
 
 }
