3393842d919f557e5ea6f734f2e4f7120639a7ca
YakovMoshel <yakovmoshel123@gmail.com>
2024-05-28 18:01:10 +0300

    feedBack mission

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,7 @@ app.use(express.json())
 import UserRouter from './routes/UserRouter'
 app.use('/user', UserRouter)
 
+import FeedBackRouter from './routes/FeedBackRouter'
+app.use('/feedback', FeedBackRouter)
+
 app.listen(3030, () => console.log("Server is UP : 3030"))
