8b555a775a3632ea57ed37e701fa72a8b91b8479
ypreiser <ypreiser@gmail.com>
2024-05-28 18:42:56 +0300

    temp

--- src/routes/MediaRouter.ts
+++ src/routes/MediaRouter.ts
@@ -1,41 +1,39 @@
-import fs from "fs"
-import multer from "multer"
-import { Request, Response, Router } from 'express'
-import { Mapper } from '../helpers/Mapper'
-const router = Router()
+import fs from "fs";
+import multer, { Multer } from "multer";
+import { Request, Response, Router } from 'express';
+import { Mapper } from '../helpers/Mapper';
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = './files';
+const router: Router = Router();
+
+const storage: multer.StorageEngine = multer.diskStorage({
+    destination: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
+        const uploadPath: string = './files';
         fs.mkdirSync(uploadPath, { recursive: true });
         cb(null, uploadPath);
     },
-    filename: function (req, file, cb) {
+    filename: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) {
         cb(null, file.originalname);
     }
 });
 
-const upload = multer({ storage: storage })
+const upload: Multer = multer({ storage: storage });
 
-router.post("/", upload.any(), (req, res) => {
+router.post("/", upload.any(), (req: Request, res: Response) => {
     try {
-        console.log(req.file); // Log information about the uploaded file
-        res.send("File uploaded successfully."); // Respond to the client indicating success
+        console.log(req.files); 
+        res.send("Files uploaded successfully."); 
     } catch (error) {
-        console.log('Error:', error); // Log any errors that occur during file upload
-        res.status(500).send("An error occurred during file upload."); // Respond with an error message to the client
+        console.log('Error:', error); 
+        res.status(500).send("An error occurred during file upload."); 
     }
-})
+});
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
     try {
-        res.send("yep")
-
+        res.send("yep");
     } catch (error) {
         console.log(error);
-
     }
-})  
-
+});
 
 export default router;
