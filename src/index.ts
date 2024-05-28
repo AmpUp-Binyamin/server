25de6bc87341b5f1fb7453a000a2d98dad9ff933
ypreiser <ypreiser@gmail.com>
2024-05-28 18:11:13 +0300

    files2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import cors from 'cors'
 import express from 'express';
 import { connect } from './config/db'
 connect()
+import fs = require("fs")
+if (!fs.existsSync('./files')) {
+    fs.mkdirSync('./files')
+    const code = fs.readFileSync('./files/code.txt', 'utf8')
+}
 
 const app = express()
 app.use(cors())
@@ -10,5 +15,8 @@ app.use(express.json())
 
 import UserRouter from './routes/UserRouter'
 app.use('/user', UserRouter)
+
+import MediaRouter from './routes/MediaRouter'
+app.use('/media', MediaRouter)
 
 app.listen(3030, () => console.log("Server is UP : 3030"))
--- src/routes/MediaRouter.ts
+++ src/routes/MediaRouter.ts
@@ -1,6 +1,41 @@
 import fs from "fs"
+import multer from "multer"
+import { Request, Response, Router } from 'express'
+import { Mapper } from '../helpers/Mapper'
+const router = Router()
 
-if (!fs.existsSync('../../files')) {
-    fs.mkdirSync('./uploads')
-}
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const uploadPath = './files';
+        fs.mkdirSync(uploadPath, { recursive: true });
+        cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
+const upload = multer({ storage: storage })
+
+router.post("/", upload.any(), (req, res) => {
+    try {
+        console.log(req.file); // Log information about the uploaded file
+        res.send("File uploaded successfully."); // Respond to the client indicating success
+    } catch (error) {
+        console.log('Error:', error); // Log any errors that occur during file upload
+        res.status(500).send("An error occurred during file upload."); // Respond with an error message to the client
+    }
+})
+
+router.get("/", (req, res) => {
+    try {
+        res.send("yep")
+
+    } catch (error) {
+        console.log(error);
+
+    }
+})  
+
+
+export default router;
