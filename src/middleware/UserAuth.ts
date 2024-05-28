c5507db970c809a523a98e191a2a0db8b0de46fc
IsraBA <IsraelBenAri1000@gmail.com>
2024-05-28 18:02:29 +0300

    Auth middleware

--- src/middleware/UserAuth.ts
+++ src/middleware/UserAuth.ts
@@ -1,18 +1,18 @@
-import IMember from "../interfaces/IMember"
+import IUser from "../interfaces/IUser"
 import UserService from "../services/UserService"
 import Permission from "../types/Permission"
 
 export default abstract class UserAuth {
     userId: string
     userPermission: Permission
-    user?: IMember | null
+    user?: IUser | null
 
     constructor() {
         this.userId = ''
         this.userPermission = 'user'
     }
 
-    async getFullData?(): Promise<IMember | null> {
+    async getFullData?(): Promise<IUser | null> {
         if (this.user) return this.user;
         const user = await UserService.getSingleUser(this.userId);
         this.user = user;
