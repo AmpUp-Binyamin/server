e22a57368b7b20b3f651ae7521ca3a362b2a23c7
unknown <miriampuny16.com>
2024-05-29 15:07:10 +0300

    update member-first

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -1,6 +1,19 @@
 import MemberController from "../controllers/MemberControllers";
 import AddMemberRequest from "../dto/member/AddMemberRequest";
+import UpdateMemberRequest from "../dto/member/UpdateMemberRequest";
 import IMember from "../interfaces/IMember";
+
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const regexPhone = /^[1-9]\d{1,14}$/;
+const regexLink = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
+
+const validation= {
+    fullName:(fullName:string)=>{if (fullName.length <= 0 ) throw 'the name is not exist'},
+    email:(email:string)=>{if (!regexEmail.test(email)) throw 'the email is not valid'},
+    phone:(phone:number)=>{if (!regexPhone.test(String(phone))) throw 'the phone is not valid'},
+    link:(link:string)=>{if (!regexLink.test(link)) throw 'the link is not valid'},
+    linkToSocialNetworks:(linkToSocialNetworks:string[])=>{linkToSocialNetworks.forEach( l=> {if (!regexLink.test(l)) throw 'there is link in list links is not valid'}) }
+}
 
 
 export default class MemberService {
@@ -10,8 +23,6 @@ export default class MemberService {
     static async getsingelMember(id: string): Promise<IMember | null> {
         return await this.controller.readOne(id)
     }
-
-    
 
     static async createNewMember(data : AddMemberRequest): Promise<IMember | null> { 
         let newMember : IMember = {
@@ -26,5 +37,16 @@ export default class MemberService {
             notifications: data.notifications
         }
         return await this.controller.create(newMember)
+    }
+
+
+    static async updateMember(data:UpdateMemberRequest): Promise< IMember | null> {
+        // בהמשך - שמירת התמונה ובדיקות בהתאם
+        // מה קורה אם קיבלתי משו לא מהטיפוס הרצוי? מי מחזיר שגיאה?
+        Object.keys(validation).forEach(key=>{
+            // if (data[key]) validation[key](data[key])
+        })
+        let member = await this.controller.update(data.userId,data)
+        return member
     }
 }
