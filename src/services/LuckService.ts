1466755d88ea10f60add8c77c96940ece8f50cb4
tehilla <tehilla183@gmail.com>
2024-06-02 12:53:13 +0300

    coach

--- src/services/LuckService.ts
+++ src/services/LuckService.ts
@@ -1,9 +1,23 @@
 import ChallengeController from "../controllers/ChallengeController";
 import LuckResponse from "../dto/luck/LuckResponse";
-import  {RandomNumberGenerator}  from "../helpers/luck";
+import { RandomNumberGenerator } from "../helpers/luck";
+// type Shape = "cherry" | "diamond" | "bell" | "clover" | "currency" | "lemon";
+
+// type Shapes = {
+//   [key in Shape]: number;
+// };
 export class LuckHelper {
   static RandomGenerator = new RandomNumberGenerator()
-  static shapes: string[] = ["a", "b", "c", "d", "e", "f"];
+  static shapes: string[] = ["cherry", "diamond", "bell", "clover", "currency", "lemon"];
+  // // דובדבן יהלום פעמון תלתן מטבע לימון
+  static getShapes = [
+    { "cherry": 20 },
+    { "diamond": 100 },
+    { "bell": 30 },
+    { "clover": 40 },
+    { "currency": 50 },
+    { "lemon": 10 },
+  ];
   static getLottery(drawProbability: number): boolean {
     return drawProbability >= this.RandomGenerator.getRandom(0, 1);
   }
