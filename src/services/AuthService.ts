7c8a9baedf07c28c593934ed0955be10c25253d9
liel_ben_him <lielcohen213@gmail.com>
2024-06-05 16:35:00 +0300

    signingoogel

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,33 +1,34 @@
 import MemberController from "../controllers/MemberControllers";
 import activeChallengeController from "../controllers/ActiveChallengeController"
-import { ObjectId } from "mongoose";
+import { ObjectId, Schema } from "mongoose";
 import { createToken } from "../middleware/auth"
 import { Code } from "mongodb";
+import IMember from "../interfaces/IMember";
+import ChallengeController from "../controllers/ChallengeController";
+import IActiveChallenge from "../interfaces/IActiveChallenge";
 
 export default class AuthService {
     static MemberController = new MemberController();
+    static ChallengeController = new ChallengeController();
+
     static async checkEmail(body: { email: string, name: string, picture: string }) {
         let fullName = body.name
         let img = body.picture
         let email = body.email
-
+        //בודק אם האימייל נמצא בממבר
         let member = (await this.MemberController.read({ email }))[0]
-        let myActivChallenge: any = []
+
+        //אם מצא בודק אם יש צורך לעדכן את התמונה והשם ופועל בהתאם
         if (member) {
             if (!member.fullName) { await this.MemberController.update(member.id, { fullName }) }
             if (!member.img) { await this.MemberController.update(member.id, { img }) }
-            let myChallenge = member.myChallenge
-            await Promise.all(myChallenge.map(async challengeId => {
-                let memberActivChaleng = await AuthService.checkActivChaleng(challengeId as ObjectId)
-                if (memberActivChaleng.length > 0) {
-                    myActivChallenge.push(memberActivChaleng[0])
-                }
-            }))
         }
 
+        //בודק אם האימייל נמצא מוזמן באתגר פעיל כלשהו
+        //אם נמצא מוזמן ולא קיים בממבר אז יוצר ממבר 
         let invited = await AuthService.findInvitedActivChaleng(email)
         if (invited.length > 0) {
-            console.log({ invited: invited });
+           
             if (!member) {
                 await this.MemberController.create({
                     email: email,
@@ -41,21 +42,64 @@ export default class AuthService {
                     myCoins: [],
                 })
             }
-            myActivChallenge.push({ invited })
         }
 
+        // לבסוף שולח את הממבר המעודכן וזורק שגיאה ספציפית למקרה שלא נמצא ממבר
         member = (await this.MemberController.read({ email }))[0]
         if (member == undefined) { throw ({ status: 407, msg: "mamber not exist" }) }
-        const token = createToken({ userId: member.id, userPermission: "user" })
-        return ({ myActivChallenge, member, token })
+
+        return ({ member })
     }
 
     static activeChallengeController = new activeChallengeController();
-
     static async checkActivChaleng(challenge: ObjectId) {
         return await this.activeChallengeController.read({ challenge })
     }
     static async findInvitedActivChaleng(email: string) {
         return await this.activeChallengeController.read({ invited: email })
     }
+    static async getMyInvitesAndMyActiveChallenge(email: string) {
+        let member = (await this.MemberController.read({ email }))[0]
+        let myActivChallenge = member.myActiveChallenge
+        let myChallenge = member.myChallenge
+        let myInvites = member.myInvites as ObjectId[] || []
+        let invitedInActivChallenge = await AuthService.findInvitedActivChaleng(email)
+        if (invitedInActivChallenge.length > 0) {
+            invitedInActivChallenge.forEach(i => {
+                
+                if ( !myInvites.find(a => a == i.id) &&
+                    !myActivChallenge.find(a => a == i.id) &&
+                    !myChallenge.find(a => a == i.id))
+                  { myInvites.push(i.id as ObjectId)}
+            })
+        }
+        member.myInvites = myInvites
+        await this.MemberController.update(member.id,  {myInvites})
+        let activChallengeOn: (Schema.Types.ObjectId | IActiveChallenge)[] = []
+        await Promise.all(myActivChallenge.map(async id => {
+            let activChallenge = await AuthService.findByIdActivChaleng(id as ObjectId)
+            let challengeID = activChallenge?.challenge
+            let startDate = activChallenge?.startDate
+            let challenge = await AuthService.findByIdChaleng(challengeID as unknown as ObjectId)
+            let duration = challenge?.duration
+            let endDate
+            if (startDate && duration) {
+                endDate = new Date(startDate);
+                endDate.setDate(endDate.getDate() + duration);
+                if (endDate > new Date()) {
+                    activChallengeOn.push(id)
+                }
+            }
+        }))
+        const token = createToken({ userId: member.id, userPermission: "user" })
+        return ({ member, invites: member.myInvites, myActivChallenge: activChallengeOn, token })
+    }
+    static async findByIdActivChaleng(id: ObjectId) {
+        return await this.activeChallengeController.readOne(id)
+    }
+    static async findByIdChaleng(id: ObjectId) {
+        return await this.ChallengeController.readOne(id)
+    }
+
+
 }
