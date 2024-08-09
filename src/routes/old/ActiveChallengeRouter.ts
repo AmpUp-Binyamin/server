07546ebe862f06ef97179d7520d4c502c4bac44b
ypreiser <ypreiser@gmail.com>
2024-08-09 08:43:05 +0300

    prize

--- src/routes/old/ActiveChallengeRouter.ts
+++ src/routes/old/ActiveChallengeRouter.ts
@@ -1,121 +1,121 @@
-import { Request, Response, Router } from "express";
-import { Mapper } from "../../helpers/Mapper";
-import ActiveChallegeService from "../../services/old/ActiveChallengeService";
-import AddActiveChallengeRequest from "../../dto/activeChallenge/AddActiveChallengeRequest";
-import { loveCard } from "../../services/old/LoveCardService";
-import IActiveChallenge from "../interfaces/IActiveChallenge";
-import GetActiveChallToStartReq from "../../dto/activeChallenge/GetActiveChallToStartReq";
-import AddUserRequest from "../../dto/user/AddUserRequest";
-import GetStatusDoneCardsRes from "../../dto/activeChallenge/GetStatusDoneCardsRes";
-import { verifyTokenCoach } from "../../middleware/coachAuth";
-import { tempMediaUpload, validateAndUploadMedia } from "../../middleware/s3";
-const router = Router();
+// import { Request, Response, Router } from "express";
+// import { Mapper } from "../../helpers/Mapper";
+// import ActiveChallegeService from "../../services/old/ActiveChallengeService";
+// import AddActiveChallengeRequest from "../../dto/activeChallenge/AddActiveChallengeRequest";
+// import { loveCard } from "../../services/old/LoveCardService";
+// import IActiveChallenge from "../interfaces/IActiveChallenge";
+// import GetActiveChallToStartReq from "../../dto/activeChallenge/GetActiveChallToStartReq";
+// import AddUserRequest from "../../dto/user/AddUserRequest";
+// import GetStatusDoneCardsRes from "../../dto/activeChallenge/GetStatusDoneCardsRes";
+// import { verifyTokenCoach } from "../../middleware/coachAuth";
+// import { tempMediaUpload, validateAndUploadMedia } from "../../middleware/s3";
+// const router = Router();
 
-router.get(
-  "/:activeChallengeId",
-  verifyTokenCoach,
-  async (req: Request, res: Response) => {
-    try {
-      let activeChallenge =
-        await ActiveChallegeService.getSingleActiveChallenge(req.params.userId);
-      console.log("hi");
+// router.get(
+//   "/:activeChallengeId",
+//   verifyTokenCoach,
+//   async (req: Request, res: Response) => {
+//     try {
+//       let activeChallenge =
+//         await ActiveChallegeService.getSingleActiveChallenge(req.params.userId);
+//       console.log("hi");
 
-      res.send(activeChallenge);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  }
-);
+//       res.send(activeChallenge);
+//     } catch (error) {
+//       res.status(400).send(error);
+//     }
+//   }
+// );
 
-router.get("/start/:activeChallengeId", async (req: Request, res: Response) => {
-  try {
-    let activeChallenge: GetActiveChallToStartReq | null =
-      await ActiveChallegeService.getActiveChallengeToStartScreen(
-        req.params.activeChallengeId
-      );
-    res.send(activeChallenge);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+// router.get("/start/:activeChallengeId", async (req: Request, res: Response) => {
+//   try {
+//     let activeChallenge: GetActiveChallToStartReq | null =
+//       await ActiveChallegeService.getActiveChallengeToStartScreen(
+//         req.params.activeChallengeId
+//       );
+//     res.send(activeChallenge);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
 
-router.get(
-  "/status/:activeChallengeId",
-  async (req: Request, res: Response) => {
-    try {
-      let userId = req.body.userId;
-      let startDeilyDeck: GetStatusDoneCardsRes =
-        await ActiveChallegeService.getStartDailyDeck(
-          userId,
-          req.params.activeChallengeId
-        );
-      res.send(startDeilyDeck);
-    } catch (error) {
-      console.log(error);
-      res.status(400).send(error);
-    }
-  }
-);
+// router.get(
+//   "/status/:activeChallengeId",
+//   async (req: Request, res: Response) => {
+//     try {
+//       let userId = req.body.userId;
+//       let startDeilyDeck: GetStatusDoneCardsRes =
+//         await ActiveChallegeService.getStartDailyDeck(
+//           userId,
+//           req.params.activeChallengeId
+//         );
+//       res.send(startDeilyDeck);
+//     } catch (error) {
+//       console.log(error);
+//       res.status(400).send(error);
+//     }
+//   }
+// );
 
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    let activeChallenge = await ActiveChallegeService.createNewActiveChallenge(
-      req.body
-    );
-    console.log(activeChallenge);
+// router.post("/", async (req: Request, res: Response) => {
+//   try {
+//     let activeChallenge = await ActiveChallegeService.createNewActiveChallenge(
+//       req.body
+//     );
+//     console.log(activeChallenge);
 
-    res.send(activeChallenge);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+//     res.send(activeChallenge);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
 
-router.get("/cardLove/:challengeId", async (req: Request, res: Response) => {
-  try {
-    let luck = await loveCard.getLove(req.params.challengeId);
-    console.log({ luck });
-    res.send(luck);
-  } catch (error) {
-    console.log(error);
+// router.get("/cardLove/:challengeId", async (req: Request, res: Response) => {
+//   try {
+//     let luck = await loveCard.getLove(req.params.challengeId);
+//     console.log({ luck });
+//     res.send(luck);
+//   } catch (error) {
+//     console.log(error);
 
-    res.status(400).send(error);
-  }
-});
+//     res.status(400).send(error);
+//   }
+// });
 
-// תשובה על קלף ספציפי
-router.post(
-  "/:activeChallengeId/card/:cardId",
-  async (req: Request, res: Response) => {
-    try {
-      let activeChallengeId = req.params.activeChallengeId;
-      let cardId = req.params.cardId;
-      await ActiveChallegeService.handleCardAnswer(
-        activeChallengeId,
-        cardId,
-        req.body
-      );
-      res.json({ success: true });
-    } catch (error) {
-      console.log({ error });
-      res.status(400).send({ error });
-    }
-  }
-);
+// // תשובה על קלף ספציפי
+// router.post(
+//   "/:activeChallengeId/card/:cardId",
+//   async (req: Request, res: Response) => {
+//     try {
+//       let activeChallengeId = req.params.activeChallengeId;
+//       let cardId = req.params.cardId;
+//       await ActiveChallegeService.handleCardAnswer(
+//         activeChallengeId,
+//         cardId,
+//         req.body
+//       );
+//       res.json({ success: true });
+//     } catch (error) {
+//       console.log({ error });
+//       res.status(400).send({ error });
+//     }
+//   }
+// );
 
-router.post("/join/:challengeId", async (req: Request, res: Response) => {
-  try {
-    let userId = req.body.userId;
-    // let userId = '665c8b2bd125fccc69966357'
-    let activeChallengeId = req.params.challengeId;
-    const result = await ActiveChallegeService.joinActiveChallenge(
-      userId,
-      activeChallengeId
-    );
-    res.send(result);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(error);
-  }
-});
+// router.post("/join/:challengeId", async (req: Request, res: Response) => {
+//   try {
+//     let userId = req.body.userId;
+//     // let userId = '665c8b2bd125fccc69966357'
+//     let activeChallengeId = req.params.challengeId;
+//     const result = await ActiveChallegeService.joinActiveChallenge(
+//       userId,
+//       activeChallengeId
+//     );
+//     res.send(result);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(400).send(error);
+//   }
+// });
 
-export default router;
+// export default router;
